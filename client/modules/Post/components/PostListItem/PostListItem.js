7000fa40688cd4b2ba41355f9bb566f07c175abb
Colin Nico Schmid <mail@mala23.me>
2018-06-03 19:36:09 +0200

    style item

--- client/modules/Post/components/PostListItem/PostListItem.js
+++ client/modules/Post/components/PostListItem/PostListItem.js
@@ -7,14 +7,14 @@ import styles from './PostListItem.css';
 
 function PostListItem(props) {
   return (
+    <Link to={`/posts/${props.post.slug}-${props.post.cuid}`} >
     <div className={styles['single-post']}>
       <img className={styles['post-icon']} src="https://s3.eu-central-1.amazonaws.com/minidini/images/icons/icon_microwave-white.svg" alt="Item Icon"/>
       <h3 className={styles['post-title']}>
-        <Link to={`/posts/${props.post.slug}-${props.post.cuid}`} >
           {props.post.title}
-        </Link>
       </h3>
     </div>
+    </Link>
   );
 }
 
