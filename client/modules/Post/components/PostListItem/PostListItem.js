746b08531012d4216cf9ca0eb641a2e5cb5e66fd
Colin Nico Schmid <mail@mala23.me>
2018-06-04 12:56:59 +0200

    change to grid

--- client/modules/Post/components/PostListItem/PostListItem.js
+++ client/modules/Post/components/PostListItem/PostListItem.js
@@ -7,7 +7,7 @@ import styles from './PostListItem.css';
 
 function PostListItem(props) {
   return (
-    <Link to={`/posts/${props.post.slug}-${props.post.cuid}`} >
+    <Link className={styles['post-link']} to={`/posts/${props.post.slug}-${props.post.cuid}`} >
     <div className={styles['single-post']}>
       <img className={styles['post-icon']} src="https://s3.eu-central-1.amazonaws.com/minidini/images/icons/icon_microwave-white.svg" alt="Item Icon"/>
       <h3 className={styles['post-title']}>
